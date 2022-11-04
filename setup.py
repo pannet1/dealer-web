fd3ead1277a988fe215b73c8098181c61158cd9e
karthick <prog@ecomsense.in>
2022-11-05 05:07:49 +0530

    added setup for pip support

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='dealer-web',
+      version='1.0',
+      description='a fastapi dealer app for indian stock market',
+      author='b karthick',
+      author_email='prog@ecomsense.in',
+      url='https://github.com/pannet1/dealer-web/',
+      packages=['dealer-web']
+      )
