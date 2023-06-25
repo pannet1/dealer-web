7371fc44151e020f97b73b5b43005c3535c25140
karthick <prog@ecomsense.in>
2023-06-25 19:30:38 +0530

    add apply to dataframe

--- dealer_web/equity.py
+++ dealer_web/equity.py
@@ -1,18 +1,21 @@
+
 import orjson
-import pandas as pd
 import pendulum
+import pandas as pd
+import numpy as np
 import concurrent.futures as cf
-import user
 from toolkit.fileutils import Fileutils
 from toolkit.utilities import Utilities
-from time import sleep
-import numpy as np
+import user
+
 
 # user preferences
-PERC = 0.03
-YESTERDAY_TMINUS = 2
-TRADE_DAY_TMINUS = 1
+Y_PERC = 0.03
+T_PERC = 0.01
 MODE_TRADE = 0
+
+YESTERDAY_TMINUS = 3
+TRADE_DAY_TMINUS = 2
 
 futil = Fileutils()
 lst_dohlcv = ["dtime", "o", "h", "l", "c", "v"]
@@ -20,6 +23,7 @@ df_empty = pd.DataFrame()
 fpath = "../../../"
 dpath = fpath + "data/"
 pd.options.mode.chained_assignment = None
+tutils = Utilities()
 
 
 class Equity:
@@ -27,17 +31,13 @@ class Equity:
     def __init__(self):
         user.contracts()
         self.df = pd.DataFrame()
-        self.sleep = 1
         self.max = pendulum.now().subtract(days=9)
         self.yday = pendulum.now().subtract(days=YESTERDAY_TMINUS)
         self.now = pendulum.now().subtract(days=TRADE_DAY_TMINUS)
 
     def is_mode_trades(self, csv):
         # testing mode
-        if MODE_TRADE == 0:
-            return True
-        # file needs modification
-        elif futil.is_file_not_2day(csv):
+        if MODE_TRADE == 0 and futil.is_file_not_2day(csv):
             return True
         return False
 
@@ -52,7 +52,6 @@ class Equity:
                 columns = ["token", "symbol"]
                 df_tok = df_tok.query("exch_seg=='NSE'")[columns].rename(
                     columns={"token": "symboltoken"})
-
                 df_sym = futil.get_df_fm_csv(
                     dpath, "nifty_200", ["symbol", "enabled"])
                 df_sym.dropna(inplace=True)
@@ -66,12 +65,12 @@ class Equity:
             print(f"{e} while set_symbols \n")
             SystemExit()
         else:
-            print(df.tail(3))
+            print(df.tail())
             return df
 
     def _get_ohlc(self, **kwargs):
         try:
-            sleep(1)
+            tutils.slp_til_nxt_sec()
             ao = user.random_broker()
             param = {
                 "exchange": "NSE",
@@ -79,29 +78,29 @@ class Equity:
             t = kwargs.pop('tminus', 0)
             param.update(kwargs)
             resp = ao.obj.getCandleData(param)
-            data = resp.get('data', None)
             if (
-                data is not None
-                and isinstance(data, list)
-                and isinstance(data[t:], list)
-                and isinstance(data[t:][0], list)
+                resp is not None
+                and isinstance(resp, dict)
+                and isinstance(resp['data'], list)
+                and isinstance(resp['data'][t:], list)
+                and isinstance(resp['data'][t:][0], list)
             ):
-                return data[t:][0]
-            else:
-                return 0
+                return resp['data'][t:][0]
+            return 0
         except Exception as e:
             print(
-                f"{str(e)} while ohlc \n sleeping for {self.sleep} sec(s)")
+                f"{str(e)} while get_ohlc \n sleeping for {tutils.sec} sec(s)")
+            tutils.slp_for(tutils.sec)
             return 0
 
     def get_eod_data(self):
         try:
             fro = self.max.set(hour=9, minute=14).to_datetime_string()[:-3]
             nto = self.yday.set(hour=15, minute=30).to_datetime_string()[:-3]
-            print(f"getting PDH {fro} to {nto}")
+            print(f"getting PDH for {nto}")
             csvfile = dpath + "2_eod_data.csv"
-            if futil.is_file_not_2day(csvfile):
-                while (not self.df.query("pdh==0").empty) and (self.sleep < 15):
+            if self.is_mode_trades(csvfile):
+                while (not self.df.query("pdh==0").empty):
                     df_cp = self.df.query("pdh==0").copy()
                     for i, row in df_cp.iterrows():
                         kwargs = {
@@ -109,7 +108,7 @@ class Equity:
                             'fromdate': fro,
                             'todate': nto,
                             'interval': 'ONE_DAY',
-                            'tminus': 1
+                            'tminus': -1
                         }
                         lst = self._get_ohlc(**kwargs)
                         if isinstance(lst, list):
@@ -120,8 +119,6 @@ class Equity:
                             print(row.symbol, str(lst))
                         else:
                             self.df.at[i, 'pdh'] = 0
-                    self.sleep += 1
-                self.sleep = 1
                 df = self.df.query("pdh>0")
                 df.to_csv(csvfile, index=False)
             else:
@@ -149,7 +146,7 @@ class Equity:
     def apply_conditions(self, df):
         print("YESTERDAY CONDITIONS")
         csvfile = dpath + "4_yday_conditions.csv"
-        combined_condition = (df['yday_perc'] > PERC) | (
+        combined_condition = (df['yday_perc'] > Y_PERC) | (
             df['dir'] == 0)
         df = df[~combined_condition]
         df.to_csv(csvfile, index=False)
@@ -157,103 +154,38 @@ class Equity:
         print(df.tail(5))
         return df
 
-    def get_ohlc(self, symboltoken, fro, nto, tf):
-        try:
-            lst_col = ["o", "h", "l", "c"]
-            empty_ohlc = pd.DataFrame(columns=lst_col, data=[[0, 0, 0, 0]])
-            ao = user.random_broker()
-            param = {
-                "exchange": "NSE",
-                "symboltoken": symboltoken,
-                "interval": "ONE_MINUTE",
-                "fromdate": fro,
-                "todate": nto,
-            }
-            resp = ao.obj.getCandleData(param)
-            # print(f"response is {resp.get('data')} for param \n {param}")
-            if resp.get('data'):
-                data = resp.get('data')
-                df = pd.DataFrame(columns=lst_dohlcv, data=data)
-                times = lst_dohlcv[0]
-                df[times] = pd.to_datetime(df[times])
-                df = df.set_index("dtime")
-                dct = {
-                    "o": "first",
-                    "h": "max",
-                    "l": "min",
-                    "c": "last"
-                }
-                t = df.groupby(pd.Grouper(freq=tf)).agg(dct)
-                t.columns = lst_col
-                t.reset_index(drop=True, inplace=True)
-                t = t.loc[[0]]
-                return t
-            else:
-                return empty_ohlc
-        except Exception as e:
-            print(f"{e} while getting ohlc")
-            return empty_ohlc
-
-    def is_low_broken(self, df):
-        if df['Low'][0] < df['Low'].min():
-            return True
-        else:
-            return False
-
-    def fltr_upr_vltd(self, df):
-        df = df.assign(upr_vltd=(df.c < df.upr) & (df.h > df.upr))
+    def gap_condtions(self, df):
+        """ 
+            used in backtesting mode
+        """
+        fro = self.now.set(hour=9, minute=15).to_datetime_string()[:-3]
+        nto = self.now.set(hour=9, minute=20).to_datetime_string()[:-3]
+        print(f"{fro} to {nto}")
+        param = {
+            "exchange": "NSE",
+            "interval": "FIVE_MINUTE",
+            "fromdate": fro,
+            "todate": nto,
+            "tminus": 0,
+        }
+        for i, row in self.df.iterrows():
+            param["symboltoken"] = row.symboltoken
+            lst = self._get_ohlc(**param)
+            if isinstance(lst, list):
+                df.at[i, 'open'] = lst[1]
+                print(row.symbol, str(lst))
         return df
 
-    def get_candles(self, param, tf):
-        try:
-            sleep(0.5)
-            lst_col = ["o", "h", "l", "c"]
-            empty_ohlc = pd.DataFrame(columns=lst_col, data=[[0, 0, 0, 0]])
-            ao = user.random_broker()
-            resp = ao.obj.getCandleData(param)
-            data = resp.get('data')
-            if data:
-                df = pd.DataFrame(columns=lst_dohlcv, data=data)
-                df.drop("v", inplace=True, axis=1)
-                times = lst_dohlcv[0]
-                df[times] = pd.to_datetime(df[times])
-                df = df.set_index("dtime")
-                dct = {
-                    "o": "first",
-                    "h": "max",
-                    "l": "min",
-                    "c": "last"
-                }
-                t = df.groupby(pd.Grouper(freq=tf)).agg(dct)
-                t.columns = lst_col
-                t = t.reset_index().rename(columns={'index': 'dtime'})
-                print(t.tail())
-                return t
-            else:
-                return empty_ohlc
-        except Exception as e:
-            print(f"{e} failed to get candles for {param['symboltoken']}")
-        else:
-            return df
-
-    def place_order(symbol, ohlc):
-        df = pd.DataFrame.from_dict(ohlc)
-        df['Datetime'] = pd.to_datetime(df['Datetime'], unit='ms')
-        df.set_index('Datetime', inplace=True)
-        yesterday_high = df['High'][-1]
-        if len(df) == 5 and is_low_broken(df) and is_closed_above_yesterday_high(df, yesterday_high) and \
-                not is_any_candle_closed_above_1st_high(df) and is_range_within_1_percent(df):
-            entry_price = df['Low'][0]
-            stop_loss = df['High'][0]
-            target_price = entry_price - (entry_price - stop_loss) * 2
-            print(
-                f"Short trade order placed for {symbol} at {entry_price} with stop loss at {stop_loss} and target price at {target_price}")
-
-    def scan_stocks(stocks, ohlc_data):
-        with cf.ThreadPoolExecutor() as executor:
-            for symbol in stocks:
-                ohlc = ohlc_data.get(symbol)
-                # executor.submit(place_order, symbol, ohlc)
+    def get_preopen(self, row):
+        ao = user.random_broker()
+        resp = ao.obj.ltpData("NSE", row.symbol, row.symboltoken)
+        if (
+            resp is not None
+            and isinstance(resp, dict)
+            and isinstance(resp['data'], dict)
+        ):
+            print(row.symbol, resp['data']['ltp'])
+            return resp['data']['ltp']
 
 
 if __name__ == "__main__":
@@ -262,30 +194,19 @@ if __name__ == "__main__":
     eqty.df = eqty.get_eod_data()
     eqty.df = eqty.eval_direction(eqty.df)
     eqty.df = eqty.apply_conditions(eqty.df)
-    """
-    GET TODAY'S DATA
-    """
-    csvfile = dpath + "6_final.csv"
-    fro = eqty.now.set(hour=9, minute=15).to_datetime_string()[:-3]
-    nto = eqty.now.set(hour=9, minute=20).to_datetime_string()[:-3]
-    print(f"{fro} to {nto}")
-    param = {
-        "exchange": "NSE",
-        "interval": "FIVE_MINUTE",
-        "fromdate": fro,
-        "todate": nto,
-        "tminus": 0,
-    }
-    df = eqty.df.copy()
-    for i, row in eqty.df.iterrows():
-        param["symboltoken"] = row.symboltoken
-        lst = eqty._get_ohlc(**param)
-        if isinstance(lst, list):
-            df.at[i, 'open'] = lst[1]
+
+    print("FINDING GAP TRADES ..", end="\n",)
+    csvfile = dpath + "5.gap_conditions.csv"
+    if MODE_TRADE == 0:
+        eqty.df['open'] = eqty.df.apply(eqty.get_preopen, axis=1)
+    else:
+        df = eqty.gap_condtions(eqty.df)
     df['perc'] = np.where(df['dir'] == 1, (df['open'] - df['pdh']) /
                           df['pdh'], (df['pdl'] - df['open']) / df['open'])
-    offshoots = df['perc'] > 0.03
-    df = df[~offshoots]
+    is_perc_grt = df['perc'] > T_PERC
+    df = df[~is_perc_grt]
+    str_perc = f"perc>0&perc<={T_PERC}"
+    df['is_gap'] = df.eval(str_perc)
     df.to_csv(csvfile, index=False)
     print(df.tail())
     """
--- dealer_web/keltner.py
+++ dealer_web/keltner.py
@@ -23,3 +23,110 @@ def get_kc(high, low, close, kc_lookback, multiplier, atr_lookback):
     # kc_lower = close.ewm(kc_lookback).mean() - multiplier * atr
 
     return kc_upper
+
+
+"""
+unused
+"""
+
+   def is_low_broken(self, df):
+        if df['Low'][0] < df['Low'].min():
+            return True
+        else:
+            return False
+
+    def fltr_upr_vltd(self, df):
+        df = df.assign(upr_vltd=(df.c < df.upr) & (df.h > df.upr))
+        return df
+
+    def get_candles(self, param, tf):
+        try:
+            sleep(0.5)
+            lst_col = ["o", "h", "l", "c"]
+            empty_ohlc = pd.DataFrame(columns=lst_col, data=[[0, 0, 0, 0]])
+            ao = user.random_broker()
+            resp = ao.obj.getCandleData(param)
+            data = resp.get('data')
+            if data:
+                df = pd.DataFrame(columns=lst_dohlcv, data=data)
+                df.drop("v", inplace=True, axis=1)
+                times = lst_dohlcv[0]
+                df[times] = pd.to_datetime(df[times])
+                df = df.set_index("dtime")
+                dct = {
+                    "o": "first",
+                    "h": "max",
+                    "l": "min",
+                    "c": "last"
+                }
+                t = df.groupby(pd.Grouper(freq=tf)).agg(dct)
+                t.columns = lst_col
+                t = t.reset_index().rename(columns={'index': 'dtime'})
+                print(t.tail())
+                return t
+            else:
+                return empty_ohlc
+        except Exception as e:
+            print(f"{e} failed to get candles for {param['symboltoken']}")
+        else:
+            return df
+
+    def get_ohlc(self, symboltoken, fro, nto, tf):
+        try:
+            lst_col = ["o", "h", "l", "c"]
+            empty_ohlc = pd.DataFrame(columns=lst_col, data=[[0, 0, 0, 0]])
+            ao = user.random_broker()
+            param = {
+                "exchange": "NSE",
+                "symboltoken": symboltoken,
+                "interval": "ONE_MINUTE",
+                "fromdate": fro,
+                "todate": nto,
+            }
+            resp = ao.obj.getCandleData(param)
+            # print(f"response is {resp.get('data')} for param \n {param}")
+            if resp.get('data'):
+                data = resp.get('data')
+                df = pd.DataFrame(columns=lst_dohlcv, data=data)
+                times = lst_dohlcv[0]
+                df[times] = pd.to_datetime(df[times])
+                df = df.set_index("dtime")
+                dct = {
+                    "o": "first",
+                    "h": "max",
+                    "l": "min",
+                    "c": "last"
+                }
+                t = df.groupby(pd.Grouper(freq=tf)).agg(dct)
+                t.columns = lst_col
+                t.reset_index(drop=True, inplace=True)
+                t = t.loc[[0]]
+                return t
+            else:
+                return empty_ohlc
+        except Exception as e:
+            print(f"{e} while getting ohlc")
+            return empty_ohlc
+
+
+    def place_order(symbol, ohlc):
+        df = pd.DataFrame.from_dict(ohlc)
+        df['Datetime'] = pd.to_datetime(df['Datetime'], unit='ms')
+        df.set_index('Datetime', inplace=True)
+        yesterday_high = df['High'][-1]
+        if len(df) == 5 and is_low_broken(df) and is_closed_above_yesterday_high(df, yesterday_high) and \
+                not is_any_candle_closed_above_1st_high(df) and is_range_within_1_percent(df):
+            entry_price = df['Low'][0]
+            stop_loss = df['High'][0]
+            target_price = entry_price - (entry_price - stop_loss) * 2
+            print(
+                f"Short trade order placed for {symbol} at {entry_price} with stop loss at {stop_loss} and target price at {target_price}")
+
+    def scan_stocks(stocks, ohlc_data):
+        with cf.ThreadPoolExecutor() as executor:
+            for symbol in stocks:
+                ohlc = ohlc_data.get(symbol)
+                # executor.submit(place_order, symbol, ohlc)
+
+
+
