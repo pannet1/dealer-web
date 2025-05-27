9aee696af6fc26955a527f376ddb27988209e92f
pannet1 <prog@ecomsense.in>
2025-05-28 02:58:48 +0530

    convert to class

--- src/monitor.py
+++ src/monitor.py
@@ -1,28 +1,28 @@
 from constants import alerts_json, futil
 from pprint import pprint
+from user import load_all_users
 
 
-def extract_alert_symbols():
-    resp = futil.read_file(alerts_json)
-    return [alert["name"]
-    for alert in resp["alerts"]]
+class Monitor:
+    def __init__(self):
+        resp = futil.read_file(alerts_json)
+        self.equities = [alert["name"] for alert in resp["alerts"]]
+        self.json_file = futil.read_file(alerts_json)
+        self.users = load_all_users()
 
-def _update_equity_ltp(equities):
-    #get random broker
-    return {}
+    def _update_equity_ltp(self):
 
-def update_alert_ltp(equities, resp):
-    equities = _update_equity_ltp(equities)
-    return resp 
+        # get random broker
+        return resp
 
-if __name__ == "__main__":
-    equities: list = extract_alert_symbols()
-    resp = futil.read_file(alerts_json)
-    while True:
-        resp = update_alert_ltp(equities, resp)
-        for alert in resp["alerts"]:
+    def run(self):
+        self._update_equity_ltp()
+        for alert in self.json_file["alerts"]:
             if alert["ltp"] > alert["above"] or alert["ltp"] < alert["below"]:
                 print("send alert")
-        
 
-    
+
+if __name__ == "__main__":
+    monitor = Monitor()
+    while True:
+        monitor.run()
